8ff577425a1abfb98c93298abe44d1fb123c6cd8
fcrizul <fcrizul@gmail.com>
2019-10-09 22:04:15 -0300

    1.0

---
+++ background.js
@@ -0,0 +1,37 @@
+chrome.webRequest.onBeforeRequest.addListener(function() {
+  		return { cancel: true };
+	}, { urls: [
+		"*://*/js/epd_sw*" // EL PAIS
+		] 
+	}, ["blocking"] 
+);
+
+chrome.cookies.onChanged.addListener(function(changeInfo) {
+	if (changeInfo.cookie.domain == 'www.elobservador.com.uy'){
+		if (changeInfo.cookie.name == 'mostrar_Signwall'){
+			// EL OBSERVADOR
+			chrome.cookies.remove({url: "https://www.elobservador.com.uy" + "/", name: "mostrar_Signwall"});
+		}
+	}
+});
+
+chrome.tabs.onUpdated.addListener( function (tabId, changeInfo, tab) {
+  if (changeInfo.status == 'complete') {
+	// EL OBSERVADOR
+	chrome.cookies.remove({url: "https://www.elobservador.com.uy" + "/", name: "mostrar_Signwall"});
+	chrome.cookies.remove({url: "https://www.elobservador.com.uy" + "/", name: "mostrar_AvisoSuscripcion"});
+	chrome.cookies.remove({url: "https://www.elobservador.com.uy" + "/", name: "mostrar_Paywall"});
+	
+	/*
+	chrome.cookies.set({
+		"name": "userId",
+		"url": "https://www.elobservador.com.uy/",
+		"value": "100"
+	}, function (cookie) {
+		console.log(JSON.stringify(cookie));
+		console.log(chrome.extension.lastError);
+		console.log(chrome.runtime.lastError);
+	});
+	*/
+  }
+})
